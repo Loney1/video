bf74ca526c95017036f5fb37f96c86ebd0773e95
yinlong <yin_long@topsec.com.cn>
2022-12-30 13:20:52 +0800

    [add] 获取广告信息

--- models/advert.go
+++ models/advert.go
@@ -14,3 +14,10 @@ type Advert struct {
 func init() {
 	orm.RegisterModel(new(Advert))
 }
+
+func GetChannelAdvert(channelId int) (int64, []Advert, error) {
+	o := orm.NewOrm()
+	var adverts []Advert
+	num, err := o.Raw("SELECT id, title, sub_title,img,add_time,url FROM advert WHERE status=1 AND channel_id=? ORDER BY sort DESC LIMIT 1", channelId).QueryRows(&adverts)
+	return num, adverts, err
+}
