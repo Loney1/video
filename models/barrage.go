e96c4eb5ec5d555d46198a05af86c95cba498d1c
yinlong <yin_long@topsec.com.cn>
2022-12-30 11:30:29 +0800

    [add] 获取并保存弹幕信息

--- models/barrage.go
+++ models/barrage.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"github.com/astaxie/beego/orm"
+	"time"
+)
 
 type Barrage struct {
 	Id          int
@@ -21,4 +24,25 @@ type BarrageData struct {
 
 func init() {
 	orm.RegisterModel(new(Barrage))
+}
+
+func BarrageList(episodesId int, startTime int, endTime int) (int64, []BarrageData, error) {
+	o := orm.NewOrm()
+	var barrages []BarrageData
+	num, err := o.Raw("SELECT id,content,`current_time` FROM barrage WHERE status=1 AND episodes_id=? AND `current_time`>=? AND `current_time`<? ORDER BY `current_time` ASC", episodesId, startTime, endTime).QueryRows(&barrages)
+	return num, barrages, err
+}
+
+func SaveBarrage(episodesId int, videoId int, currentTime int, userId int, content string) error {
+	o := orm.NewOrm()
+	var barrage Barrage
+	barrage.Content = content
+	barrage.CurrentTime = currentTime
+	barrage.AddTime = time.Now().Unix()
+	barrage.UserId = userId
+	barrage.Status = 1
+	barrage.EpisodesId = episodesId
+	barrage.VideoId = videoId
+	_, err := o.Insert(&barrage)
+	return err
 }
