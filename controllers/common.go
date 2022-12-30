15fd0c400ed34eca7358f2715038876db92971dd
yinlong <yin_long@topsec.com.cn>
2022-12-30 11:27:27 +0800

    [add] 日志处理

--- controllers/common.go
+++ controllers/common.go
@@ -6,3 +6,20 @@ import "github.com/astaxie/beego"
 type CommonController struct {
 	beego.Controller
 }
+
+type JsonStruct struct {
+	Code  int         `json:"code"`
+	Msg   interface{} `json:"msg"`
+	Items interface{} `json:"items"`
+	Count int64       `json:"count"`
+}
+
+func ReturnSuccess(code int, msg interface{}, items interface{}, count int64) (json *JsonStruct) {
+	json = &JsonStruct{Code: code, Msg: msg, Items: items, Count: count}
+	return
+}
+
+func ReturnError(code int, msg interface{}) (json *JsonStruct) {
+	json = &JsonStruct{Code: code, Msg: msg}
+	return
+}
