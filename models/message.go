22c42e7f9518841a34da52ebfdeda7803abc0555
yinlong <yin_long@topsec.com.cn>
2023-01-03 21:13:28 +0800

    [add] 消息队列处理

--- models/message.go
+++ models/message.go
@@ -1,6 +1,12 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"encoding/json"
+	"time"
+	"video/services/mq"
+
+	"github.com/astaxie/beego/orm"
+)
 
 type Message struct {
 	Id      int
@@ -18,4 +24,28 @@ type MessageUser struct {
 
 func init() {
 	orm.RegisterModel(new(Message), new(MessageUser))
+}
+
+//保存通知消息
+func SendMessageDo(content string) (int64, error) {
+	o := orm.NewOrm()
+	var message Message
+	message.Content = content
+	message.AddTime = time.Now().Unix()
+	messageId, err := o.Insert(&message)
+	return messageId, err
+}
+
+//保存消息接收人到队列中
+func SendMessageUserMq(userId int, messageId int64) {
+	//把数据转换成json字符串
+	type Data struct {
+		UserId    int
+		MessageId int64
+	}
+	var data Data
+	data.UserId = userId
+	data.MessageId = messageId
+	dataJson, _ := json.Marshal(data)
+	mq.Publish("", "video_send_message_user", string(dataJson))
 }
