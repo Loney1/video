25b0ca02761b65e48b14c53261950d08c096f287
yinlong <yin_long@topsec.com.cn>
2023-01-03 21:11:49 +0800

    [add] 处理用户信息

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"github.com/astaxie/beego/orm"
+	"time"
+)
+
 type User struct {
 	Id       int
 	Name     string
@@ -14,4 +19,51 @@ type UserInfo struct {
 	Name    string `json:"name"`
 	AddTime int64  `json:"addTime"`
 	Avatar  string `json:"avatar"`
+}
+
+//根据手机号判断用户是否存在
+func IsUserMobile(mobile string) bool {
+	o := orm.NewOrm()
+	user := User{Mobile: mobile}
+	err := o.Read(&user, "Mobile")
+	if err == orm.ErrNoRows {
+		return false
+	} else if err == orm.ErrMissPK {
+		return false
+	}
+	return true
+}
+
+//保存用户
+func UserSave(mobile string, password string) error {
+	o := orm.NewOrm()
+	var user User
+	user.Name = ""
+	user.Password = password
+	user.Mobile = mobile
+	user.Status = 1
+	user.AddTime = time.Now().Unix()
+	_, err := o.Insert(&user)
+	return err
+}
+
+//登录功能
+func IsMobileLogin(mobile string, password string) (int, string) {
+	o := orm.NewOrm()
+	var user User
+	err := o.QueryTable("user").Filter("mobile", mobile).Filter("password", password).One(&user)
+	if err == orm.ErrNoRows {
+		return 0, ""
+	} else if err == orm.ErrMissPK {
+		return 0, ""
+	}
+	return user.Id, user.Name
+}
+
+//根据用户ID获取用户信息
+func GetUserInfo(uid int) (UserInfo, error) {
+	o := orm.NewOrm()
+	var user UserInfo
+	err := o.Raw("SELECT id,name,add_time,avatar FROM user WHERE id=? LIMIT 1", uid).QueryRow(&user)
+	return user, err
 }
